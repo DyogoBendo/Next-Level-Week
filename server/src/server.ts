0e560a10368293303a72eea5233b7241fdfa9d6c
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-06-03 22:05:10 -0300

    CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express, { request, response } from 'express';
-import path from 'path';
+import path from 'path'; // facilitador de navegacao de pastas
 import routes from './routes'; // importando as rotas
+import cors from 'cors'; // Adiciona o cors para nossa aplicacao
 
 // Ao usar TypeScript, as bibliotecas precisam vir com as definicoes de tipo
 
 const app = express(); // Crio a aplicacao principal, usando express
 
+app.use(cors);
 app.use(express.json()); // express nao vem para trabalhar apenas com JSON, precisamos explicitar
 app.use(routes); // Digo para meu app usar as rotas
 
